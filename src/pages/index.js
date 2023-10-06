16ac25de86b386f8ee3c143a8fa1f8d55b23f0da
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-10-07 04:14:48 +0530

    feat: header added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-
-
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout noFooter description="Description will go into a meta tag in <head />">
       <main>
+        <section className="bg-gray-900">
+          <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 lg:py-16 lg:grid-cols-12">
+            <div className="mr-auto place-self-center lg:col-span-7">
+              <h1 className="max-w-xl mb-4 text-3xl font-extrabold tracking-wide lg:tracking-tight leading-none lg:text-5xl text-white">Self-hosted Lightweight PaaS solution</h1>
+              <p className="max-w-xl mb-6 font-light text-gray-400 lg:mb-8 md:text-lg lg:text-xl">Easily simplify your application deployment and VPS management, all without the hassle</p>
+              <div className="flex flex-col lg:flex-row gap-2 lg:gap-5 mt-10">
+                <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-900 no-underline hover:no-underline">
+                  Get started
+                  <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                  </svg>
+                </a>
+                <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center border-solid border-2 border-white rounded-lg hover:bg-gray-700 focus:ring-8 text-white no-underline hover:no-underline">
+                  <svg className="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                  </svg>
+                  Star on GitHub
+                </a>
+              </div>
+            </div>
+            <div className="mt-5 lg:mt-0 lg:col-span-5 lg:flex rounded-md overflow-hidden">
+              <img src="/img/architecture.gif" className="w-fit" alt="mockup" />
+            </div>
+          </div>
+        </section>
       </main>
     </Layout>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: { 50: "#f5f3ff", 100: "#ede9fe", 200: "#ddd6fe", 300: "#c4b5fd", 400: "#a78bfa", 500: "#8b5cf6", 600: "#7c3aed", 700: "#6d28d9", 800: "#5b21b6", 900: "#4c1d95", 950: "#2e1065" },
+      },
+    },
+    fontFamily: {
+      body: ["Inter"],
+      sans: ["Inter"],
+    },
+  },
+  plugins: [],
+};
