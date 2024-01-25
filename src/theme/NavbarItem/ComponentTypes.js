723cffc3243f8ad44422d1a0c518e126708db334
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-01-26 02:12:19 +0530

    chore: github button added (#14)

---
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import GithubStarButton from './GithubStarButton';
+
+export default {
+  ...ComponentTypes,
+  'custom-github-star-btn': GithubStarButton,
+};
---
+++ src/theme/NavbarItem/GithubStarButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import GitHubButton from 'react-github-btn'
+
+export default function () {
+    return (
+        <GitHubButton href="https://github.com/swiftwave-org/swiftwave" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" data-show-count="true" aria-label="Star swiftwave-org/swiftwave on GitHub">Star</GitHubButton>
+    );
+}
